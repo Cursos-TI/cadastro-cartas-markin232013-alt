63d51c3de7853544f21506504420342e2b7784d6
markin232013-alt <markin232013@gmail.com>
2025-09-27 17:12:00 +0000

    carta 01

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,50 @@
 
 int main() {
   
-  printf("Desafio Super Trunfo\n");
+  printf("Carta 01\n");
+  
+  char Estado[50]; 
+  char Codigo;
+  char Cidade;
+  int População; 
+  float Area; 
+  float PIB;
+  int turisticos;
+
+  
+  
+  printf("Estado: \n");
+  scanf("%s", Estado);
+
+  printf("Código: \n");
+  scanf("%s", &Codigo);
+
+  printf("Cidade: \n");
+  scanf("%s", &Cidade);
+
+  printf("População: \n");
+  scanf("%d", &População);
+
+  printf("Area: \n");
+  scanf("%f", &Area);
+
+  printf("PIB: \n");
+  scanf("%f", &PIB);
+
+  printf("Numero de Pontos Turisticos: \n");
+  scanf("%d", &turisticos);
+
+
+
+
+
+
+
+
+
 
   
 
   
 
-}
+};
