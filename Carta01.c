c3eda91e2955e37d76a73fa60028eaa2df0360e7
markin232013-alt <markin232013@gmail.com>
2025-09-29 14:51:05 +0000

    DESAFIO SUPER TRUNFO CARTA01 E 02

--- Carta01.c
+++ Carta01.c
@@ -3,8 +3,9 @@
 
 int main() {
 
+printf("Cartas Super Trunfo \n");
 
- printf("Carta 01\n");
+printf("Carta 01\n");
   
   char Estado[50]; 
   char Codigo;
@@ -13,34 +14,61 @@ int main() {
   float Area; 
   float PIB;
   int turisticos;
+ 
+printf("Estado 01: \n");
+scanf("%s", Estado);
 
-  
-  
-  printf("Estado 01: \n");
-  scanf("%s", Estado);
+printf("Código 01: \n");
+scanf("%s", &Codigo);
 
-  printf("Código 01: \n");
-  scanf("%s", &Codigo);
+printf("Cidade 01: \n");
+scanf("%s", &Cidade);
 
-  printf("Cidade 01: \n");
-  scanf("%s", &Cidade);
+printf("População 01: \n");
+scanf("%d", &População);
 
-  printf("População 01: \n");
-  scanf("%d", &População);
+printf("Area 01: \n");
+scanf("%f", &Area);
 
-  printf("Area 01: \n");
-  scanf("%f", &Area);
+printf("PIB 01: \n");
+scanf("%f", &PIB);
 
-  printf("PIB 01: \n");
-  scanf("%f", &PIB);
-
-  printf("Numero de Pontos Turisticos 01: \n");
-  scanf("%d", &turisticos);
+printf("Numero de Pontos Turisticos 01: \n");
+scanf("%d", &turisticos);
 
 
+printf("Carta 02\n"); 
+
+ char Estado [50];
+ char Codigo;
+ char Cidade;
+ int Populaçao;
+ float Area;
+ float PIB;
+ int Turisticos;
+
+printf("Estado 02: \n");
+scanf("%s", Estado);
+
+printf("Codigo 02: \n");
+scanf("%s", &Codigo);
+
+printf("cidade 02: \n");
+scanf("%s", &Cidade);
+
+printf("Polulaçao 02: \n");
+scanf("%d", &Populaçao);
+
+printf("Area 02: \n");
+scanf("%f", &Area);
+
+printf("PIB 02: \n");
+scanf("%f", &PIB);
+
+printf("Numero de Pontos Turisticos 02: \n");
+scanf("%d", &Turisticos);
 
 
-
-
+return 0;
 
 }
