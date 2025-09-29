119587a899e2ee69996a069b915b67576b52633d
markin232013-alt <markin232013@gmail.com>
2025-09-29 13:46:29 +0000

    cartas 01 e 02

---
+++ Carta02.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+
+printf("Carta02\n"); 
+
+char Estado [50];
+char Codigo;
+char Cidade;
+int Populaçao;
+float Area;
+float PIB;
+int Turisticos;
+
+printf("Estado: \n");
+scanf("%s", Estado);
+
+printf("Codigo: \n");
+scanf("%s", &Codigo);
+
+printf("cidade: \n");
+scanf("%s", &Cidade);
+
+printf("Polulaçao: \n");
+scanf("%d", &Populaçao);
+
+printf("Area: \n");
+scanf("%f", &Area);
+
+printf("PIB: \n");
+scanf("%f", PIB);
+
+printf("Numero de Pontos Turisticos: \n");
+scanf("%d", &Turisticos);
+
+
+
+
+
+}
